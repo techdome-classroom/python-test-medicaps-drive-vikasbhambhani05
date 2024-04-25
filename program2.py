6f31043670ebe761eafd6322dd889f2af189676a
vikas <vikas.bhambhani0005@gmail.com>
2024-04-25 13:56:26 +0530

    updated files

--- program2.py
+++ program2.py
@@ -1,7 +1,17 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev_value = 0
+
+        for char in s:
+            current_value = roman_values[char]
+            total += current_value
+
+            if current_value > prev_value:
+                total -= 2 * prev_value
+
+            prev_value = current_value
+
+        return total
